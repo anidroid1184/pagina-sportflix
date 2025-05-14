a3b75d4831cc40ec8fb5de0b9047ab49b4198c91
Anidroid 118 <juan.sebastian.2004.a@gmail.com>
2025-05-14 13:48:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1746983441231.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    cambiala por public/images/logo.png

--- src/components/icons/SporflixLogo.tsx
+++ src/components/icons/SporflixLogo.tsx
@@ -1,15 +1,22 @@
 // src/components/icons/SporflixLogo.tsx
-import type { SVGProps } from 'react';
+import Image from 'next/image';
+import type { HTMLAttributes } from 'react';
 
-export function SporflixLogo(props: SVGProps<SVGSVGElement>) {
+interface SporflixLogoProps extends HTMLAttributes<HTMLDivElement> {
+  width?: number;
+  height?: number;
+  alt?: string;
+}
+
+export function SporflixLogo({ width = 28, height = 28, alt = "SPORFLIX Logo", className, ...props }: SporflixLogoProps) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100" // Assuming a square viewBox, adjust if needed
-      fill="currentColor"
-      {...props}
-    >
-      <path d="M85.39,14.61L14.61,85.39L14.61,75.73C14.61,75.73 20.12,64.75 30.82,60.56C41.52,56.37 50.67,58.48 50.67,58.48L50.67,49.33C50.67,49.33 43.63,41.95 38.27,30.82C32.92,19.69 34.31,14.61 34.31,14.61L49.33,14.61L49.33,41.52C49.33,41.52 56.37,44.36 61.73,50.67C67.08,56.98 65.69,69.18 65.69,69.18L65.69,85.39L85.39,85.39L85.39,14.61Z" />
-    </svg>
+    <div className={className} {...props} style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        src="/images/logo.png"
+        alt={alt}
+        width={width}
+        height={height}
+      />
+    </div>
   );
 }
